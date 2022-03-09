c572b72f4d82bf9085515d0e083c5e4cb4ca84a3
Ali <alicoalico78@gmail.com>
2022-03-09 16:49:00 +0200

    login and signup commit

--- lib/view/screens/auth/login_screen.dart
+++ lib/view/screens/auth/login_screen.dart
@@ -17,137 +17,139 @@ class LoginScreen extends StatelessWidget {
         elevation: 0,
       ),
       backgroundColor: Colors.grey.shade300,
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Image(
-                image: AssetImage('assets/images/logo.png'),
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height * 0.25,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextUtils(
-                text: 'عيادتي',
-                color: Colors.blue.shade900,
-                fontSize: 30,
-                fontWeight: FontWeight.bold),
-            TextUtils(
-                text: 'My clinic',
-                color: Colors.blue.shade900,
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
-            SizedBox(
-              height: 50,
-            ),
-            TextUtils(
-                text: 'Sign In to your account',
-                color: Colors.blue.shade900,
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
-            SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                elevation: 5,
-                child: Container(
-                  height: 35,
-                  child: AuthTextFormField(
-                      controller: emailController,
-                      obsecure: false,
-                      validator: (value) {},
-                      hinttext: ' your name'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Image(
+                  image: AssetImage('assets/images/logo.png'),
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.25,
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                elevation: 5,
-                child: Container(
-                  height: 35,
-                  child: AuthTextFormField(
-                      controller: passwordController,
-                      obsecure: true,
-                      validator: (value) {},
-                      hinttext: ' your password'),
-                ),
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Container(
-              width: 130,
-              height: 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                gradient: LinearGradient(colors: [
-                  Colors.green,
-                  Colors.blue.shade900,
-                ], begin: Alignment.topLeft, end: Alignment.topRight),
+              TextUtils(
+                  text: 'عيادتي',
+                  color: Colors.blue.shade900,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+              TextUtils(
+                  text: 'My clinic',
+                  color: Colors.blue.shade900,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+              SizedBox(
+                height: 50,
               ),
-              child: AuthButton(
-                text: 'Sign in',
-                onPressed: () {},
-                color: Colors.transparent,
+              TextUtils(
+                  text: 'Sign In to your account',
+                  color: Colors.blue.shade900,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+              SizedBox(
+                height: 15,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (_) => SignUpScreen()));
-              },
-              style: TextButton.styleFrom(
-                padding: EdgeInsets.zero,
-              ),
-              child: TextUtils(
-                text: 'Or create account',
-                color: Colors.grey,
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image(
-                  image: AssetImage('assets/images/facebook.png'),
-                ),
-                SizedBox(
-                  width: 3,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: TextUtils(
-                    text: 'Or sign in with facebook',
-                    color: Colors.blue.shade900,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
                   ),
-                )
-              ],
-            )
-          ],
+                  elevation: 5,
+                  child: Container(
+                    height: 35,
+                    child: AuthTextFormField(
+                        controller: emailController,
+                        obsecure: false,
+                        validator: (value) {},
+                        hinttext: ' your name'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  elevation: 5,
+                  child: Container(
+                    height: 35,
+                    child: AuthTextFormField(
+                        controller: passwordController,
+                        obsecure: true,
+                        validator: (value) {},
+                        hinttext: ' your password'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: 130,
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: LinearGradient(colors: [
+                    Colors.green,
+                    Colors.blue.shade900,
+                  ], begin: Alignment.topLeft, end: Alignment.topRight),
+                ),
+                child: AuthButton(
+                  text: 'Sign in',
+                  onPressed: () {},
+                  color: Colors.transparent,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (_) => SignUpScreen()));
+                },
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                ),
+                child: TextUtils(
+                  text: 'Or create account',
+                  color: Colors.grey,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage('assets/images/facebook.png'),
+                  ),
+                  SizedBox(
+                    width: 3,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: TextUtils(
+                      text: 'Or sign in with facebook',
+                      color: Colors.blue.shade900,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/view/screens/auth/signup_screen.dart
+++ lib/view/screens/auth/signup_screen.dart
@@ -18,159 +18,161 @@ class SignUpScreen extends StatelessWidget {
         elevation: 0,
       ),
       backgroundColor: Colors.grey.shade300,
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Image(
-                image: AssetImage('assets/images/logo.png'),
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height * 0.25,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextUtils(
-                text: 'عيادتي',
-                color: Colors.blue.shade900,
-                fontSize: 30,
-                fontWeight: FontWeight.bold),
-            TextUtils(
-                text: 'My clinic',
-                color: Colors.blue.shade900,
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
-            SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                elevation: 5,
-                child: Container(
-                  height: 35,
-                  child: AuthTextFormField(
-                      controller: emailController,
-                      obsecure: false,
-                      validator: (value) {},
-                      hinttext: ' your email'),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                elevation: 5,
-                child: Container(
-                  height: 35,
-                  child: AuthTextFormField(
-                      controller: emailController,
-                      obsecure: false,
-                      validator: (value) {},
-                      hinttext: ' your password'),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                elevation: 5,
-                child: Container(
-                  height: 35,
-                  child: AuthTextFormField(
-                      controller: emailController,
-                      obsecure: false,
-                      validator: (value) {},
-                      hinttext: ' your address'),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                elevation: 5,
-                child: Container(
-                  height: 35,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Image(
+                  image: AssetImage('assets/images/logo.png'),
                   width: double.infinity,
-                  child: DropdownButtonHideUnderline(
-                    child: Container(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                      child: DropdownButton<String>(
-                        hint: TextUtils(
-                            text: 'your specialty',
-                            color: Colors.grey.shade400,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold),
-                        value: _value,
-                        items: _items.map((value) {
-                          return DropdownMenuItem<String>(
-                              child: Text(value), value: value);
-                        }).toList(),
-                        onChanged: (val) {},
+                  height: MediaQuery.of(context).size.height * 0.25,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextUtils(
+                  text: 'عيادتي',
+                  color: Colors.blue.shade900,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+              TextUtils(
+                  text: 'My clinic',
+                  color: Colors.blue.shade900,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+              SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  elevation: 5,
+                  child: Container(
+                    height: 35,
+                    child: AuthTextFormField(
+                        controller: emailController,
+                        obsecure: false,
+                        validator: (value) {},
+                        hinttext: ' your email'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  elevation: 5,
+                  child: Container(
+                    height: 35,
+                    child: AuthTextFormField(
+                        controller: emailController,
+                        obsecure: false,
+                        validator: (value) {},
+                        hinttext: ' your password'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  elevation: 5,
+                  child: Container(
+                    height: 35,
+                    child: AuthTextFormField(
+                        controller: emailController,
+                        obsecure: false,
+                        validator: (value) {},
+                        hinttext: ' your address'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  elevation: 5,
+                  child: Container(
+                    height: 35,
+                    width: double.infinity,
+                    child: DropdownButtonHideUnderline(
+                      child: Container(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: DropdownButton<String>(
+                          hint: TextUtils(
+                              text: 'your specialty',
+                              color: Colors.grey.shade400,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                          value: _value,
+                          items: _items.map((value) {
+                            return DropdownMenuItem<String>(
+                                child: Text(value), value: value);
+                          }).toList(),
+                          onChanged: (val) {},
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              width: 130,
-              height: 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                gradient: LinearGradient(colors: [
-                  Colors.green,
-                  Colors.blue.shade900,
-                ], begin: Alignment.topLeft, end: Alignment.topRight),
+              SizedBox(
+                height: 30,
               ),
-              child: AuthButton(
-                text: 'Sign in',
-                onPressed: () {},
-                color: Colors.transparent,
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image(
-                  image: AssetImage('assets/images/facebook.png'),
+              Container(
+                width: 130,
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: LinearGradient(colors: [
+                    Colors.green,
+                    Colors.blue.shade900,
+                  ], begin: Alignment.topLeft, end: Alignment.topRight),
                 ),
-                SizedBox(
-                  width: 3,
-                ),
-                TextButton(
+                child: AuthButton(
+                  text: 'Sign in',
                   onPressed: () {},
-                  child: TextUtils(
-                    text: 'Or sign up with facebook',
-                    color: Colors.blue.shade900,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
+                  color: Colors.transparent,
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage('assets/images/facebook.png'),
                   ),
-                )
-              ],
-            )
-          ],
+                  SizedBox(
+                    width: 3,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: TextUtils(
+                      text: 'Or sign up with facebook',
+                      color: Colors.blue.shade900,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
